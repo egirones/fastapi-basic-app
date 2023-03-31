175d4e37b51aafb1cade72659e4edaa4fd78663f
egirones <esgirones@gmail.com>
2023-03-31 12:09:18 +0200

    changes in sanity check

--- sanitycheck.py
+++ sanitycheck.py
@@ -1,9 +1,3 @@
-"""
-Name: Unknownw
-Date: 21.03.2023
-Sanity check
-"""
-
 from os import path
 
 import argparse
@@ -12,15 +6,15 @@ import inspect
 import os
 import sys
 
-fail_color = '\033[91m'
-ok_color = '\033[92m'
-warn_color = '\033[93m'
+FAIL_COLOR = '\033[91m'
+OK_COLOR = '\033[92m'
+WARN_COLOR = '\033[93m'
 
 
 def run_sanity_check(test_dir):
 
     # assert path.isdir(test_dir), 
-    # fail_color+f"No direcotry named {test_dir} found in {os.getcwd()}"
+    # FAIL_COLOR+f"No direcotry named {test_dir} found in {os.getcwd()}"
     print('This script will perform a sanity test \
           to ensure your code meets the criteria in the rubric.\n')
     print('Please enter the path to the file that')
@@ -60,132 +54,132 @@ def run_sanity_check(test_dir):
             test_function_names))
 
     print("\n============= Sanity Check Report ===========")
-    sanity_test_passing = True
-    warning_count = 1
+    SANITY_TEST_PASSING = True
+    WARNING_COUNT = 1
 
     # GET()
-    test_for_get_method_response_code = False
-    test_for_get_method_response_body = False
+    TEST_FOR_GET_METHOD_RESPONSE_CODE = False
+    TEST_FOR_GET_METHOD_RESPONSE_BODY = False
     if not test_functions_for_get:
-        print(fail_color + f"[{warning_count}]")
-        warning_count += 1
-        print(fail_color + "No test cases were detected \
+        print(FAIL_COLOR + f"[{WARNING_COUNT}]")
+        WARNING_COUNT += 1
+        print(FAIL_COLOR + "No test cases were detected \
               for the GET() method.")
         print(
-            fail_color +
+            FAIL_COLOR +
             "\nPlease make sure you have a test case for the GET method.\
             This MUST test both the status code as well as the contents \
             of the request object.\n")
-        sanity_test_passing = False
+        SANITY_TEST_PASSING = False
 
     else:
         for func in test_functions_for_get:
             source = inspect.getsource(getattr(module, func))
             if source.find('.status_code') != -1:
-                test_for_get_method_response_code = True
+                TEST_FOR_GET_METHOD_RESPONSE_CODE = True
             if (source.find('.json') != -1) or (source.find('json.loads') != -1):
-                test_for_get_method_response_body = True
+                TEST_FOR_GET_METHOD_RESPONSE_BODY = True
 
-        if not test_for_get_method_response_code:
-            print(fail_color + f"[{warning_count}]")
-            warning_count += 1
+        if not TEST_FOR_GET_METHOD_RESPONSE_CODE:
+            print(FAIL_COLOR + f"[{WARNING_COUNT}]")
+            WARNING_COUNT += 1
             print(
-                fail_color +
+                FAIL_COLOR +
                 "Your test case for GET() does not seem to \
                 be testing the response code.\n")
 
-        if not test_for_get_method_response_body:
-            print(fail_color + f"[{warning_count}]")
-            warning_count += 1
+        if not TEST_FOR_GET_METHOD_RESPONSE_BODY:
+            print(FAIL_COLOR + f"[{WARNING_COUNT}]")
+            WARNING_COUNT += 1
             print(
-                fail_color +
+                FAIL_COLOR +
                 "Your test case for GET() does not seem to \
                     be testing the CONTENTS of the response.\n")
 
     # POST()
-    test_for_post_method_response_code = False
-    test_for_post_method_response_body = False
-    count_post_method_for_inference_result = 0
+    TEST_FOR_POST_METHOD_RESPONSE_CODE = False
+    TEST_FOR_POST_METHOD_RESPONSE_BODY = False
+    COUNT_POST_METHOD_TEST_FOR_INFERENCE_RESULT = 0
 
     if not test_functions_for_post:
-        print(fail_color + f"[{warning_count}]")
-        warning_count += 1
-        print(fail_color + "No test cases were detected\
+        print(FAIL_COLOR + f"[{WARNING_COUNT}]")
+        WARNING_COUNT += 1
+        print(FAIL_COLOR + "No test cases were detected\
             for the POST() method.")
         print(
-            fail_color +
+            FAIL_COLOR +
             "Please make sure you have TWO test cases\
                 for the POST() method." +
             "\nOne test case for EACH of the possible\
                 inferences (results/outputs) of the ML model.\n")
-        sanity_test_passing = False
+        SANITY_TEST_PASSING = False
     else:
         if len(test_functions_for_post) == 1:
-            print(f"[{warning_count}]")
-            warning_count += 1
+            print(f"[{WARNING_COUNT}]")
+            WARNING_COUNT += 1
             print(
-                fail_color +
+                FAIL_COLOR +
                 "Only one test case was detected\
                     for the POST() method.")
             print(
-                fail_color +
+                FAIL_COLOR +
                 "Please make sure you have two test\
                     cases for the POST() method." +
                 "\nOne test case for EACH of the possible]\
                     inferences (results/outputs) of the ML model.\n")
-            sanity_test_passing = False
+            SANITY_TEST_PASSING = False
 
         for func in test_functions_for_post:
             source = inspect.getsource(getattr(module, func))
             if source.find('.status_code') != -1:
-                test_for_post_method_response_code = True
+                TEST_FOR_POST_METHOD_RESPONSE_CODE = True
             if (source.find('.json') != -1) or (source.find('json.loads') != -1):
-                test_for_post_method_response_body = True
-                count_post_method_for_inference_result += 1
+                TEST_FOR_POST_METHOD_RESPONSE_BODY = True
+                COUNT_POST_METHOD_TEST_FOR_INFERENCE_RESULT += 1
 
-        if not test_for_post_method_response_code:
-            print(fail_color + f"[{warning_count}]")
-            warning_count += 1
+        if not TEST_FOR_POST_METHOD_RESPONSE_CODE:
+            print(FAIL_COLOR + f"[{WARNING_COUNT}]")
+            WARNING_COUNT += 1
             print(
-                fail_color +
+                FAIL_COLOR +
                 "One or more of your test cases for POST()\
                     do not seem to be testing the response code.\n")
-        if not test_for_post_method_response_body:
-            print(fail_color + f"[{warning_count}]")
-            warning_count += 1
+        if not TEST_FOR_POST_METHOD_RESPONSE_BODY:
+            print(FAIL_COLOR + f"[{WARNING_COUNT}]")
+            WARNING_COUNT += 1
             print(
-                fail_color +
+                FAIL_COLOR +
                 "One or more of your test cases for POST() do\
                     not seem to be testing the contents of the response.\n")
 
         if len(
                 test_functions_for_post
-            ) >= 2 and count_post_method_for_inference_result < 2:
-            print(fail_color + f"[{warning_count}]")
-            warning_count += 1
+            ) >= 2 and COUNT_POST_METHOD_TEST_FOR_INFERENCE_RESULT < 2:
+            print(FAIL_COLOR + f"[{WARNING_COUNT}]")
+            WARNING_COUNT += 1
             print(
-                fail_color +
+                FAIL_COLOR +
                 "You do not seem to have TWO separate test cases,\
                     one for each possible prediction that your model can make.")
 
-    sanity_test_passing = sanity_test_passing and\
-        test_for_get_method_response_code and \
-        test_for_get_method_response_body and \
-        test_for_post_method_response_code and \
-        test_for_post_method_response_body and \
-        count_post_method_for_inference_result >= 2
+    SANITY_TEST_PASSING = SANITY_TEST_PASSING and\
+        TEST_FOR_GET_METHOD_RESPONSE_CODE and \
+        TEST_FOR_GET_METHOD_RESPONSE_BODY and \
+        TEST_FOR_POST_METHOD_RESPONSE_CODE and \
+        TEST_FOR_POST_METHOD_RESPONSE_BODY and \
+        COUNT_POST_METHOD_TEST_FOR_INFERENCE_RESULT >= 2
 
-    if sanity_test_passing:
-        print(ok_color + "Your test cases look good!")
+    if SANITY_TEST_PASSING:
+        print(OK_COLOR + "Your test cases look good!")
 
     print(
-        warn_color +
+        WARN_COLOR +
         "This is a heuristic based sanity testing and cannot\
             guarantee the correctness of your code.")
     print(
-        warn_color +
-        "You should still check your work against the rubric\
-            to ensure you meet the criteria.")
+        WARN_COLOR +
+        "You should still check your work against the\
+            rubric to ensure you meet the criteria.")
 
 
 if __name__ == "__main__":
